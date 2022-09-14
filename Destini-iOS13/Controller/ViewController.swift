aa53bbae5a1be5456c1b11e42b6900668d49a467
yusheng593 <yusheng593@gmail.com>
2022-09-14 23:07:47 +0800

    故事可以持續進行

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -25,9 +25,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func choiceBtnPressed(_ sender: UIButton) {
+        let stories = storyBrain.arrStories
+        switch sender.currentTitle {
+        case stories[0].choice1:
+            updateUI(Destination: 2)
+        case stories[0].choice2:
+            updateUI(Destination: 1)
+        case stories[1].choice1:
+            updateUI(Destination: 2)
+        case stories[1].choice2:
+            updateUI(Destination: 3)
+        case stories[2].choice1:
+            updateUI(Destination: 5)
+        case stories[2].choice1:
+            updateUI(Destination: 4)
+        case stories[3].choice1:
+            updateUI(Destination: 0)
+        default:
+            updateUI(Destination: 0)
+        }
         
     }
     
+    func updateUI(Destination: Int) {
+        storyLabel.text = storyBrain.arrStories[Destination].title
+        choice1Button.setTitle(storyBrain.arrStories[Destination].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.arrStories[Destination].choice2, for: .normal)
+    }
 
 
 }
